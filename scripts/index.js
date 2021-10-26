c7fdecb3f9bf61686f013dfe7d100daa4aacc0f3
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2021-10-26 10:57:11 +0600

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Sep 20, 2021',
-  name: 'Hema Apu'
+  birthdate: 'Oct 26, 2021',
+  name: 'Ayon'
 };
 
 function hideEverything() {
